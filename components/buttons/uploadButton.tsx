ec7b591f2954e0fb121e49ebc761205969a54415
Erik <erickjosueuribe@gmail.com>
2024-09-05 08:45:19 -0500

    adjusting the menu

--- components/buttons/uploadButton.tsx
+++ components/buttons/uploadButton.tsx
@@ -3,38 +3,46 @@
 import React, { useState } from 'react';
 import { CldUploadWidget } from 'next-cloudinary';
 import useFormStore from '../../app/dashboard/store';
+import InputAlert from '../alerts/successAlert';
 
 const FormWithImageUpload: React.FC = () => {
     const { name, price, cost, setName, setPrice, setCost, setImageUrl } = useFormStore();
     const [localImageUrl, setLocalImageUrl] = useState('');
 
     const handleUploadSuccess = (result: any) => {
-        setLocalImageUrl(result.info.secure_url);
-        setImageUrl(result.info.secure_url);
+        // Verificación para asegurarse de que `result.info` y `secure_url` estén presentes
+        if (result && result.info && result.info.secure_url) {
+            setLocalImageUrl(result.info.secure_url);
+            setImageUrl(result.info.secure_url);
+        } else {
+            console.error('Error: No se encontró secure_url en el resultado de la carga.');
+        }
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        // Aquí se accede al estado actual de la tienda y se envía el formulario
-        fetch('http://localhost:8001/menu', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name,
-                price,
-                cost,
-                imageUrl: localImageUrl,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Form submitted:', data);
-            })
-            .catch((error) => {
-                console.error('Error submitting form:', error);
+        try {
+            const response = await fetch('http://localhost:8001/menu', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    price,
+                    cost,
+                    imageUrl: localImageUrl,
+                }),
             });
+
+            const data = await response.json();
+            console.log('Form submitted:', data);
+
+            // Mostrar la alerta de éxito
+            InputAlert("El producto fue añadido exitosamente", "success");
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
     };
 
     return (
@@ -75,7 +83,7 @@ const FormWithImageUpload: React.FC = () => {
                         <button
                             type="button"
                             className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-                            onClick={() => open()} // Envolver la llamada a open en una función de flecha
+                            onClick={() => open()}
                         >
                             Cargar imagen
                         </button>
