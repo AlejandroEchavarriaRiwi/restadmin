cc69e92e490b880a2b84a8d514f4eeafcdb89962
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-09-16 22:15:27 -0500

    fix titles

--- components/KitchenDashboard.tsx
+++ components/KitchenDashboard.tsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect, useRef, ReactNode } from 'react';
-import styled from 'styled-components';
-import { useReactToPrint } from 'react-to-print';
-import { CheckCircle, ClipboardList, Printer, Utensils } from 'lucide-react';
+import React, { useState, useEffect, useRef, ReactNode } from "react";
+import styled from "styled-components";
+import { useReactToPrint } from "react-to-print";
+import { CheckCircle, ClipboardList, Printer, Utensils } from "lucide-react";
+import { FaKitchenSet } from "react-icons/fa6";
 
 interface KitchenTicket {
   id: string;
@@ -21,6 +22,31 @@ interface KitchenTicket {
   observations: string;
 }
 
+const NavBar = styled.nav`
+  background-color: #f8f9fa;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    h1 {
+      margin-left: 0;
+    }
+    div {
+      flex-direction: row;
+      margin-bottom: 10px;
+      gap: 10px;
+      margin-right: 0;
+    }
+  }
+`;
+
 const DashboardContainer = styled.div`
   padding: 20px;
   max-width: 100%;
@@ -36,7 +62,7 @@ const TicketCard: React.FC<{ children: ReactNode }> = ({ children }) => (
   <div className="w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mb-4">
     {children}
   </div>
-)
+);
 const ObservationText = styled.p`
   font-style: italic;
   color: #555;
@@ -52,7 +78,7 @@ const TicketHeader: React.FC<{ children: ReactNode }> = ({ children }) => (
       <span>{children}</span>
     </h2>
   </div>
-)
+);
 
 const ItemList: React.FC<{ children: ReactNode }> = ({ children }) => (
   <div className="p-6 flex flex-col items-center">
@@ -64,29 +90,30 @@ const ItemList: React.FC<{ children: ReactNode }> = ({ children }) => (
       {children}
     </ul>
   </div>
-)
+);
 
 const ItemListItem: React.FC<{ children: ReactNode }> = ({ children }) => (
   <li>{children}</li>
-)
+);
 
-const Button: React.FC<{ onClick: () => void; children: ReactNode }> = ({ onClick, children }) => (
+const Button: React.FC<{ onClick: () => void; children: ReactNode }> = ({
+  onClick,
+  children,
+}) => (
   <button
     onClick={onClick}
     className="px-4 py-2 border border-amarillo rounded-md text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex items-center justify-center w-full mb-2"
   >
     {children}
   </button>
-)
-
-
+);
 
 const PrintableTicket = styled.div`
   padding: 10mm;
   font-family: Arial, sans-serif;
   text-align: center;
   margin: 10px;
-  h2{
+  h2 {
     font-size: large;
     font-weight: bold;
   }
@@ -106,7 +133,7 @@ export default function KitchenDashboard() {
 
   const fetchTickets = async () => {
     try {
-      const response = await fetch('http://localhost:8001/kitchen');
+      const response = await fetch("http://localhost:8001/kitchen");
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -125,16 +152,16 @@ export default function KitchenDashboard() {
     try {
       // Remove ticket from kitchen
       await fetch(`http://localhost:8001/kitchen/${ticketId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
 
       // Update table state
       await fetch(`http://localhost:8001/tables/${tableId}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ state: 'Ocupada' }),
+        body: JSON.stringify({ state: "Ocupada" }),
       });
 
       // Refresh tickets
@@ -148,68 +175,81 @@ export default function KitchenDashboard() {
     return (
       <>
         {ticket.observations && (
-          <ObservationText>Observaciones generales: {ticket.observations}</ObservationText>
+          <ObservationText>
+            Observaciones generales: {ticket.observations}
+          </ObservationText>
         )}
-        {ticket.items.map((item, index) => (
-          item.observations && (
-            <ObservationText  key={index}>
-              Observaciones para {item.name}: {item.observations}
-            </ObservationText>
-          )
-        ))}
+        {ticket.items.map(
+          (item, index) =>
+            item.observations && (
+              <ObservationText key={index}>
+                Observaciones para {item.name}: {item.observations}
+              </ObservationText>
+            )
+        )}
       </>
     );
   };
 
   return (
-    <DashboardContainer>
-      <div className="bg-primary text-primary-foreground py-2 px-2 mb-5 rounded-xl">
-        <div className="flex justify-center">
-          <h1 className="text-2xl font-bold mb-4 sm:mb-0">Cocina</h1>
+    <>
+      <NavBar>
+        <div className="flex items-center ">
+          <FaKitchenSet className="text-3xl text-gray-800" />
+          <h1 className="ml-4 text-gray-800">Cocina</h1>
         </div>
-      </div>
-      <TicketGrid>
-        {tickets.map((ticket) => (
-          <TicketCard key={ticket.id}>
-            <TicketHeader>Mesa {ticket.tableId}</TicketHeader>
-            <ItemList>
-              {ticket.items.map((item, index) => (
-                <ItemListItem key={index}>
-                  {item.quantity}x {item.name}
-                </ItemListItem>
-              ))}
-            </ItemList>
-            {renderObservations(ticket)}
-            <div className="px-6 py-4">
-              <Button onClick={handlePrint}>
-                <Printer className="h-4 w-4 mr-2 text-amarillo" />
-                Imprimir Ticket
-              </Button>
-              <Button onClick={() => completeTicket(ticket.id, ticket.tableId)}>
-                <CheckCircle className="h-4 w-4 mr-2 text-green-500" />
-                Marcar como Completado
-              </Button>
-            </div>
-          </TicketCard>
-        ))}
-      </TicketGrid>
-      <div style={{ display: 'none' }}>
-        <PrintableTicket ref={printRef}>
+      </NavBar>
+      <DashboardContainer>
+        <div className="bg-primary text-primary-foreground py-2 px-2 mb-5 rounded-xl">
+          <div className="flex justify-center">
+            <h1 className="text-2xl font-bold mb-4 sm:mb-0"></h1>
+          </div>
+        </div>
+        <TicketGrid>
           {tickets.map((ticket) => (
-            <div key={ticket.id}>
-              <h2>Mesa {ticket.tableId}</h2>
+            <TicketCard key={ticket.id}>
+              <TicketHeader>Mesa {ticket.tableId}</TicketHeader>
               <ItemList>
                 {ticket.items.map((item, index) => (
-                  <li key={index}>
+                  <ItemListItem key={index}>
                     {item.quantity}x {item.name}
-                  </li>
+                  </ItemListItem>
                 ))}
               </ItemList>
               {renderObservations(ticket)}
-            </div>
+              <div className="px-6 py-4">
+                <Button onClick={handlePrint}>
+                  <Printer className="h-4 w-4 mr-2 text-amarillo" />
+                  Imprimir Ticket
+                </Button>
+                <Button
+                  onClick={() => completeTicket(ticket.id, ticket.tableId)}
+                >
+                  <CheckCircle className="h-4 w-4 mr-2 text-green-500" />
+                  Marcar como Completado
+                </Button>
+              </div>
+            </TicketCard>
           ))}
-        </PrintableTicket>
-      </div>
-    </DashboardContainer>
+        </TicketGrid>
+        <div style={{ display: "none" }}>
+          <PrintableTicket ref={printRef}>
+            {tickets.map((ticket) => (
+              <div key={ticket.id}>
+                <h2>Mesa {ticket.tableId}</h2>
+                <ItemList>
+                  {ticket.items.map((item, index) => (
+                    <li key={index}>
+                      {item.quantity}x {item.name}
+                    </li>
+                  ))}
+                </ItemList>
+                {renderObservations(ticket)}
+              </div>
+            ))}
+          </PrintableTicket>
+        </div>
+      </DashboardContainer>
+    </>
   );
 }
