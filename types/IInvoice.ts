3df0d4a883fdda151ffc5bd73a0c214c2a63ba0a
Erik <erickjosueuribe@gmail.com>
2024-09-24 18:42:08 -0500

    improvig invoice

--- types/IInvoice.ts
+++ types/IInvoice.ts
@@ -19,3 +19,35 @@ export interface PreInvoice {
     total: number;
     date: string;
 }
+
+export interface Product {
+    Id: number;
+    Name: string;
+    Price: number;
+    Quantity: number;
+    ImageURL: string;
+    Category: {
+        Id: number;
+        Name: string;
+    };
+    Status: number;
+}
+
+export interface Order {
+    Id: number;
+    TablesId: number | null;
+    TableName: string | null;
+    Status: number;
+    Products: Product[];
+    Observations: string;
+}
+
+export interface Company {
+    Id: number;
+    Name: string;
+    Email: string;
+    Nit: string;
+    Phone: string;
+    Address: string;
+    LogoURL: string;
+}
